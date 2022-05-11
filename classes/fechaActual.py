57179d5786b6a0b00c83abd64e1cc052e78c291f
4k3d <l21940251@tlajomulco.tecnm.mx>
2022-05-10 22:44:09 -0500

    Eliminé archivos que no necesitaba

--- classes/fechaActual.py
+++
@@ -1,6 +0,0 @@
-import argparse
-from calculaFecha import CalculaFecha #En caso de error, revisar la ruta del archivo calculaFecha
-
-if __name__ == "__main__":
-	obj1 = CalculaFecha(7,5,2022)
-	obj1.calcularFecha(365)
